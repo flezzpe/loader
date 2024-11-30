85a04d5e55b072dbbd5afc93e66012330f8b63a1
flezzpe <98888240+flezzpe@users.noreply.github.com>
2024-11-30 19:52:00 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,11 @@
-warn('recive:', script_key)
+
+
+local loader = setmetatable({
+  __init__ = function()
+    setfenv(1, setmetatable({script_key = key}, {__index = getfenv()}))
+
+    warn(script_key)
+  end
+}, loader)
+
+loader.__init__()
